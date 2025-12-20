80003dc302980f771715e5cf9f601c15b3273fa9
shaileshpurohit <shaileshpurohit9427@gmail.com>
2025-12-20 22:15:28 +0530

    sending post requests

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const app = express();
 
 // app.use(bodyParser.urlencoded({ extended: false })); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+
+// Use feed routes for any requests to /feed
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(3090);
