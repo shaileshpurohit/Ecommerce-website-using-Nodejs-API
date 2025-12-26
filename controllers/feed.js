ec0c774a5df73c6447e704c93015fd98e8587f9b
shaileshpurohit <shaileshpurohit9427@gmail.com>
2025-12-26 23:04:00 +0530

    user login & creting json web token & auth

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,15 +4,27 @@ const path = require("path");
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
+const post = require("../models/post");
 exports.getPosts = (req, res, next) => {
-  Post.find()
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+  post
+    .find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
       res.status(200).json({
         message: "Posts fetched successfully.",
         posts: posts,
+        totalItems: totalItems,
       });
     })
-
     .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
